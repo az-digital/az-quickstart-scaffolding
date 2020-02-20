1a99a879c1f987e3efbe5266e1bc6c5ef0f9998c
Kevin Cooper <kevincooper@email.arizona.edu>
2020-02-20 12:19:43 -0700

    copy and update our dependency check script from d7

---
+++ scripts/ci/dependency-audit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#===============================================================================
+#
+# AZ Quickstart composer dependency checks.
+#
+# https://github.com/sensiolabs/security-checker
+#
+#===============================================================================
+
+EXIT_STATUS=0
+
+if [[ -z ${1} ]]; then
+  BASE_DIR='.'
+else
+  BASE_DIR="${1}"
+fi
+
+while read -r lockfile; do
+  echo -e "\nchecking ${lockfile}\n"
+  if ! ./vendor/bin/security-checker security:check "${lockfile}"; then
+    EXIT_STATUS=1
+  fi
+done < <(find "${BASE_DIR}" -name composer.lock)
+
+exit ${EXIT_STATUS}
